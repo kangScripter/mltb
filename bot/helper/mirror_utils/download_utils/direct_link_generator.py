c3b864f276df680be112c02eb28e0b834c015698
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-08-02 16:29:28 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -648,29 +648,20 @@ def mdis_k(urlx):
        return sendMessage(link, bot, message)
 
 def dlbypass(url: str) -> str:
-    DOMAIN = 'https://yoshare.net'
-    
-    client = cloudscraper.create_scraper(interpreter='nodejs', allow_brotli=False)
-    res = client.get(url)
-
-    h = {'referer': DOMAIN}
-    res = client.get(url, headers=h)
-
-    bs4 = BeautifulSoup(res.content, 'lxml')
-    inputs = bs4.find_all('input')
-    data = { input.get('name'): input.get('value') for input in inputs }
-
-    h = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-requested-with': 'XMLHttpRequest'
-    }
-    p = urlparse(url)
-    final_url = f'{p.scheme}://{p.netloc}/links/go'
-
-    time.sleep(3.1)
-    res = client.post(final_url, data=data, headers=h).json()
-
-    return res["url"]
+    api = "https://api.emilyx.in/api"
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    resp = client.get(url)
+    if resp.status_code == 404:
+        return "File not found/The link you entered is wrong!"
+    try:
+        resp = client.post(api, json={"type": "droplink", "url": url})
+        res = resp.json()
+    except BaseException:
+        return "API UnResponsive / Invalid Link !"
+    if res["success"] is True:
+        return res["url"]
+    else:
+        return res["msg"]
 
 
 WETRANSFER_API_URL = "https://wetransfer.com/api/v4/transfers"
