9e3edc32bee6bd0f9bcb9b7c1f6610a753d6a9e3
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-08-02 20:57:26 +0530

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -93,7 +93,7 @@ def _watch(bot, message, isZip=False, isLeech=False, multi=0):
         msg = str(e).replace('<', ' ').replace('>', ' ')
         return sendMessage(tag + " " + msg, bot, message)
     if 'entries' in result:
-        for i in ['144', '240', '360', '480', '720', '1080', '1440', '2160','6192']:
+        for i in ['144', '240', '360', '480', '720', '1080', '1440', '2160']:
             video_format = f"bv*[height<={i}][ext=mp4]"
             buttons.sbutton(f"{i}-mp4", f"qu {msg_id} {video_format} t")
             video_format = f"bv*[height<={i}][ext=webm]"
