c93ba32c43dc4109d4fa29b1b1273fff035ceb9d
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-08-02 19:52:37 +0530

    Update youtube_dl_download_helper.py

--- bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+++ bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
@@ -63,9 +63,8 @@ class YoutubeDLHelper:
                      'allow_multiple_audio_streams': True,
                      'trim_file_name': 200,
                      'noprogress': True,
-                     'user-agent': 'JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio'
-                     'ffmpeg_location': '/bin/new-api'
-                    }
+                     'user-agent': 'JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio', 
+                     'ffmpeg_location': '/bin/new-api'}
 
     @property
     def download_speed(self):
