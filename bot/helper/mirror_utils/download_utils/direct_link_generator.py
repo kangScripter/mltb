fc71e8d4c679bd788412cb6efa42b9d41d0dcffe
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-08-27 13:54:39 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -661,17 +661,8 @@ def ouo(url: str) -> str:
         return res["msg"]
 
 def htp(url: str) -> str:
-    api = "https://api.emilyx.in/api"
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    resp = client.get(url)
-    if resp.status_code == 404:
-        return "File not found/The link you entered is wrong!"
-    try:
-        resp = client.post(api, json={"type": "gplinks", "url": url})
-        res = resp.json()
-    except BaseException:
-        return "API UnResponsive / Invalid Link !"
-    if res["success"] is True:
-        return res["url"]
-    else:
-        return res["msg"]
+    download = rget(url, stream=True, allow_redirects=False) 
+       try: 
+              return download.headers["location"]
+       except:
+              return wronglink
