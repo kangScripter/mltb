df2010d9f48d46ccbbe0f73bfcf9e01001067e4d
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-14 18:11:47 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -11,7 +11,6 @@ import requests
 import re
 import time
 import cloudscraper
-import concurrent.futures
 from cfscrape import create_scraper
 from lxml import etree
 from requests import get as rget, head as rhead, post as rpost, Session as rsession
@@ -799,40 +798,34 @@ def gt(url):
     except: return "Something went wrong :("
 
 def psm(url):
-	client = cloudscraper.create_scraper(allow_brotli=False)
-	r = client.get(url)
-	soup = BeautifulSoup(r.text, "html.parser").find_all(class_="dropshadowboxes-drop-shadow dropshadowboxes-rounded-corners dropshadowboxes-inside-and-outside-shadow dropshadowboxes-lifted-both dropshadowboxes-effect-default")
-	
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		for link in soup:
-			try:
-				exit_gate = link.a.get("href")
-				executor.submit(try2link_scrape, exit_gate)
-			except: pass
+       client = requests.Session()
+       h = {
+             'upgrade-insecure-requests': '1',
+             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+       }
+       res = client.get(url, cookies={}, headers=h)
 
-def try2link_scrape(url):
-	client = cloudscraper.create_scraper(allow_brotli=False)	
-	h = {
-	'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-	}
-	res = client.get(url, cookies={}, headers=h)
-	url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
-	print(try2link_bypass(url))
+       url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
 
-def try2link_bypass(url):
-	client = cloudscraper.create_scraper(allow_brotli=False)
-	
-	url = url[:-1] if url[-1] == '/' else url
-	
-	params = (('d', int(time.time()) + (60 * 4)),)
-	r = client.get(url, params=params, headers= {'Referer': 'https://newforex.online/'})
-	
-	soup = BeautifulSoup(r.text, 'html.parser')
-	inputs = soup.find(id="go-link").find_all(name="input")
-	data = { input.get('name'): input.get('value') for input in inputs }	
-	time.sleep(7)
-	
-	headers = {'Host': 'try2link.com', 'X-Requested-With': 'XMLHttpRequest', 'Origin': 'https://try2link.com', 'Referer': url}
-	
-	bypassed_url = client.post('https://try2link.com/links/go', headers=headers,data=data)
-	return bypassed_url.json()["url"]
+       res = client.head(url)
+
+       id = re.findall('d=(.*?)&', res.headers['location'])[0]
+       id = base64.b64decode(id).decode('utf-8')
+
+       url += f'/?d={id}'
+       res = client.get(url)
+
+       bs4 = BeautifulSoup(res.content, 'html.parser')
+       inputs = bs4.find_all('input')
+       data = { input.get('name'): input.get('value') for input in inputs }
+    
+       time.sleep(6.5)
+       res = client.post(
+           'https://try2link.com/links/go',
+          headers={
+                 'referer': url,
+                 'x-requested-with': 'XMLHttpRequest',
+          }, data=data
+       )
+       out = res.json()['url'].replace('\/','/')
+       return out
