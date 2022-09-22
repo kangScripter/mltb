dd6ed8606ae716d00b4ae6bf50c3ae5e78f645cd
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-22 18:36:09 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -282,7 +282,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by @Trooporiginals',
             'mimeType': mime_type,
         }
         if parent_id is not None:
