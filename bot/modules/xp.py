e8e9d990cc46a9c9ce0ad5e647b1d689865c5902
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-25 14:00:51 +0530

    Update xp.py

--- bot/modules/xp.py
+++ bot/modules/xp.py
@@ -19,14 +19,26 @@ SHORTENER = "urlshortx.com"
 SHORTENER_API = "8fabf1c36bcaf7fb959b360ac8574f39815ae901"
 
 def _xp(message, bot):
-     args = message.text.split(" ", maxsplit=1)
-     reply_to = message.reply_to_message
-     if len(args) > 1:
-         link = args[1]
-     elif reply_to is not None:
-          link = reply_to.text
-     else:
-          link = ''
+     args = message.text.split()
+    reply_to = message.reply_to_message
+    link = ''
+    multi = 0
+    if len(args) > 1:
+        link = args[1].strip()
+        if link.strip().isdigit():
+            multi = int(link)
+            link = ''
+        elif message.from_user.username:
+            tag = f"@{message.from_user.username}"
+        else:
+            tag = message.from_user.mention_html(message.from_user.first_name)
+    if reply_to:
+        if len(link) == 0:
+            link = reply_to.text.split(maxsplit=1)[0].strip()
+        if reply_to.from_user.username:
+            tag = f"@{reply_to.from_user.username}"
+        else:
+            tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)
      cget = create_scraper().get
      xpurl = cget(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={link}&format=text').text
      reply = f"<b>xpshort-Jack<code>\n</b>{xpurl}</code>\n"
