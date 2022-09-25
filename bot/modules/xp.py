fbd18c8cce8b8f2fd99a5a7663befdb85d7bf4bd
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-25 12:30:54 +0530

    Update xp.py

--- bot/modules/xp.py
+++ bot/modules/xp.py
@@ -7,15 +7,11 @@ from bs4 import BeautifulSoup
 from telegram.ext import CommandHandler
 from urllib.parse import urlparse
 from telegram import message
-from bot.modules.clone import _clone
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot import LOGGER, dispatcher, bot
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import sendMessage
-from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
-from bot.helper.mirror_utils.download_utils.direct_link_generator import direct_link_generator
-from bot.helper.ext_utils.bot_utils import is_gp_link, is_rock_link, is_gdtot_link
 from cfscrape import create_scraper
 
 SHORTENER = "urlshortx.com"
@@ -30,12 +26,11 @@ def gplink(update, context):
           link = reply_to.text
      else:
           link = ''
-     if is_gp_link(link):
-          link = direct_link_generator(link)
-          reply = f"*gplink_bypassed-Jack*\n<code>{link}</code>\n"
-          LOGGER.info(f"Generated link: {link}")
-          return sendMessage(reply, context.bot, update.message)
-
+     cget = create_scraper().get
+     xpurl = cget(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={link}&format=text').text
+     reply = f"*xpshort-Jack*\n<code>{xpurl}</code>\n"
+     LOGGER.info(f"Generated link: {xpurl}")
+     return sendMessage(reply, context.bot, update.message)
 
 XP_HANDLER = CommandHandler(BotCommands.xpCommand, xplink, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
