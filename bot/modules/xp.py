110f3503a3fb9e4a545dd0faf243700e46429043
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-25 15:42:38 +0530

    Update xp.py

--- bot/modules/xp.py
+++ bot/modules/xp.py
@@ -47,9 +47,7 @@ def _xp(message, bot):
         reply = f"<b>xpshort-Jack</b>:<code>{xpurl}</code>\n"
         LOGGER.info(f"Generated link: {xpurl}")
         return sendMessage(reply, bot, message)
-    else:
-        sendMessage("give any link ", bot, message)
-    if multi > 1:
+        if multi > 1:
         sleep(4)
         nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
         msg = message.text.split(maxsplit=mi+1)
@@ -57,7 +55,9 @@ def _xp(message, bot):
         nextmsg = sendMessage(" ".join(msg), bot, nextmsg)
         nextmsg.from_user.id = message.from_user.id
         sleep(4)
-        Thread(target=_xp, args=(bot, nextmsg, _xp)).start()
+        Thread(target= -xp, args=(bot, nextmsg, _xp)).start()
+    else:
+        sendMessage("give any link ", bot, message)
         
 @new_thread
 def xplink(update, context):
