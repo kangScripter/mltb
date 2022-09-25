d1f854c19759be11bad8edbd9fa47ca68ffabba3
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-25 15:47:53 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -197,15 +197,6 @@ def _clone(message, bot):
             LOGGER.info(f'Cloning Done: {name}')
     else:
         sendMessage("Again clone the above link or invalid link", bot, message)
-    if multi > 1:
-        sleep(4)
-        nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
-        msg = message.text.split(maxsplit=mi+1)
-        msg[mi] = f"{multi - 1}"
-        nextmsg = sendMessage(" ".join(msg), bot, nextmsg)
-        nextmsg.from_user.id = message.from_user.id
-        sleep(4)
-        Thread(target=_xp, args=(bot, nextmsg)).start()
 
 @new_thread
 def cloneNode(update, context):
