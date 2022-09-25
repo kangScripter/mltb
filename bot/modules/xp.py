f401d4deb34d38b80b18d49ccd00b5ae1e177a4d
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-25 13:11:22 +0530

    Update xp.py

--- bot/modules/xp.py
+++ bot/modules/xp.py
@@ -32,6 +32,6 @@ def xplink(update, context):
      LOGGER.info(f"Generated link: {xpurl}")
      return sendMessage(reply, context.bot, update.message)
 
-XP_HANDLER = CommandHandler(BotCommands.XpCommand, xplink, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+xp_handler = CommandHandler(BotCommands.XpCommand, xplink, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
-dispatcher.add_handler(XP_HANDLER)
+dispatcher.add_handler(xp_handler)
