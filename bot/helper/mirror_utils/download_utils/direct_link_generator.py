12b68362d75b25d193714c0858182682f3aeb4af
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-11-07 12:43:11 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -942,17 +942,4 @@ def htpm(url):
     r = client.get(url, allow_redirects=True).text
     j = r.split('("')[-1]
     url = j.split('")')[0]
-    param = url.split("/")[-1]
-    DOMAIN = "https://go.theforyou.in"
-    final_url = f"{DOMAIN}/{param}"
-    resp = client.get(final_url)
-    soup = BeautifulSoup(resp.content, "html.parser")    
-    try: inputs = soup.find(id="go-link").find_all(name="input")
-    except: return "Incorrect Link"
-    data = { input.get('name'): input.get('value') for input in inputs }
-    h = { "x-requested-with": "XMLHttpRequest" }
-    time.sleep(10)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()['url']
-    except: return "Something went wrong :("
+    return url
